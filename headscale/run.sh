d8868a0f4e79569c6452aa89c1249c143783839b
Vinicius Yoata <vinicius.yoata@gmail.com>
2025-07-28 13:28:13 -0300

    nome

--- headscale/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-# Garante que a pasta de configuração existe
-mkdir -p /config/headscale
-# Inicia o Headscale usando o config.yaml no /config
-exec headscale serve --config /config/headscale/config.yaml
