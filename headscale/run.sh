b0948fd8b3c4ccd42d778c7b5cc37fe126158d1e
Vinicius Yoata <vinicius.yoata@gmail.com>
2025-07-29 16:35:51 -0300

    v0.2.7

--- headscale/run.sh
+++ headscale/run.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "[DEBUG] Gerando config.json em /config/headscale.json..."
-echo "$CONFIG_JSON" > /config/headscale.json
+echo "$CONFIG_YAML" > /config/headscale.yaml
 
-echo "[DEBUG] Conteúdo de /config/headscale.json:"
-cat /config/headscale.json
-
-echo "[DEBUG] Iniciando Headscale..."
-exec /usr/local/bin/headscale serve --config /config/headscale.json
+exec /usr/local/bin/headscale serve --config /config/headscale.yaml
