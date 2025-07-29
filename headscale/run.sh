79a237120d95937610c024adb8a7a8008d99d9a6
Vinicius Yoata <vinicius.yoata@gmail.com>
2025-07-29 16:30:46 -0300

    v0.2.6

--- headscale/run.sh
+++ headscale/run.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 set -e
 
-echo "[DEBUG] Iniciando run.sh..."
+echo "[DEBUG] Gerando config.json em /config/headscale.json..."
+echo "$CONFIG_JSON" > /config/headscale.json
 
-cat <<EOF > /config/headscale_config.yaml
-server_url: "${SERVER_URL}"
-listen_addr: "${LISTEN_ADDR}"
-grpc_listen_addr: "${GRPC_LISTEN_ADDR}"
-db_type: "${DB_TYPE}"
-db_path: "${DB_PATH}"
-tls_cert_path: "/ssl/fullchain.pem"
-tls_key_path: "/ssl/privkey.pem"
-EOF
-
-echo "[DEBUG] Arquivo /config/headscale_config.yaml gerado:"
-cat /config/headscale_config.yaml
+echo "[DEBUG] Conteúdo de /config/headscale.json:"
+cat /config/headscale.json
 
 echo "[DEBUG] Iniciando Headscale..."
-exec /usr/local/bin/headscale serve --config /config/headscale_config.yaml
+exec /usr/local/bin/headscale serve --config /config/headscale.json
